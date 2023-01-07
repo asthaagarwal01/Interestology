e018d518cbc55171634f1ab2fe8f7729aa0932a0
ASTHA AGARWAL <86525559+asthaagarwal01@users.noreply.github.com>
2023-01-07 12:57:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+function down()
+{
+    document.getElementById("sublisto").classList.toggle("show");
+ }
+ function down2()
+ {
+    document.getElementById("sublistc").classList.toggle("show");
+    document.getElementById("locks").innerHTML="Select Interest";
+ }
+ function down3()
+ {
+    document.getElementById("sublistt").classList.toggle("show");
+    document.getElementById("locks").innerHTML="Select Interest";
+ }
+ function down4()
+ {
+    document.getElementById("sublistp").classList.toggle("show");
+    document.getElementById("locks").innerHTML="Select Interest";
+ }
+ function down5()
+ {
+    document.getElementById("sublistr").classList.toggle("show");
+    document.getElementById("locks").innerHTML="Select Interest";
+ }
+ function down6()
+ {
+    document.getElementById("c1").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down7()
+ {
+    document.getElementById("c2").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down8()
+ {
+    document.getElementById("c3").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down9()
+ {
+    document.getElementById("c4").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down10()
+ {
+    document.getElementById("t1").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down11()
+ {
+    document.getElementById("t2").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down12()
+ {
+    document.getElementById("t3").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down13()
+ {
+    document.getElementById("t4").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down14()
+ {
+    document.getElementById("p1").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down15()
+ {
+    document.getElementById("p2").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down16()
+ {
+    document.getElementById("p3").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down17()
+ {
+    document.getElementById("p4").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down18()
+ {
+    document.getElementById("r1").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down19()
+ {
+    document.getElementById("r2").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down20()
+ {
+    document.getElementById("r3").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
+ function down21()
+ {
+    document.getElementById("r4").classList.toggle("show");
+    document.getElementById("locks2").innerHTML="Select Resource";
+ }
